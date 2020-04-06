bc886164a16ff07567a8b9a5abf58d70215a9b5f
Jeffrey Chen <jeffreyca16@gmail.com>
2020-04-05 20:38:17 -0400

    Update rotating cow scene

--- A5-Project/Assets/rotating-cows.lua
+++ A5-Project/Assets/rotating-cows.lua
@@ -18,52 +18,6 @@ box_width = 10.0
 box_length = 20.0
 fudge = 0.1
 
-back_plane = gr.nh_plane('back_plane')
-back_plane:scale(box_width, box_height, 1)
-back_plane:translate(0, 0, -box_length)
---scene_root:add_child(back_plane)
-back_plane:set_material(gold_mat)
-
-behind_plane = gr.nh_plane('behind_plane')
-behind_plane:scale(box_width, box_height, 1)
-behind_plane:translate(0, 0, fudge)
---scene_root:add_child(behind_plane)
-behind_plane:set_material(white_mat)
-
-top_plane = gr.nh_plane('top_plane')
-top_plane:scale(box_width, box_length, 1)
-top_plane:rotate('X', 90)
-top_plane:translate(0, box_height / 2.0 - fudge, -10.0)
---scene_root:add_child(top_plane)
-top_plane:set_material(white_mat)
-
-bottom_plane = gr.nh_plane('bottom_plane')
-bottom_plane:scale(box_width, box_length, 1)
-bottom_plane:rotate('X', 90)
-bottom_plane:translate(0, -box_height / 2.0 + fudge, -10.0)
---scene_root:add_child(bottom_plane)
-bottom_plane:set_material(white_mat)
-
-left_plane = gr.nh_plane('left_plane')
-left_plane:scale(box_length, box_height, 1)
-left_plane:rotate('Y', 90)
-left_plane:translate(-5, 0, -10.0)
---scene_root:add_child(left_plane)
-left_plane:set_material(blue_mat)
-
-right_plane = gr.nh_plane('right_plane')
-right_plane:scale(box_length, box_height, 1)
-right_plane:rotate('Y', -90)
-right_plane:translate(5, 0, -10.0)
---scene_root:add_child(right_plane)
-right_plane:set_material(red_mat)
-
-s = gr.sphere('s')
---scene_root:add_child(s)
-s:set_material(mirror_mat)
-s:scale(0.7, 0.7, 0.7)
-s:translate(-1.0, -3.0, -12.0)
-
 cow = gr.normal_mesh('cow', 'cow-normal.obj')
 scene_root:add_child(cow)
 cow:set_material(red_mat)
